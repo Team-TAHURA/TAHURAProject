dd9ddfb578ee9d627bc29d47371800f0bb1b3b51
Ahocheese <ranadi423putrawan@gmail.com>
2024-07-27 10:39:50 +0800

    A

--- src/app/detail-page/detail-page.component.ts
+++ src/app/detail-page/detail-page.component.ts
@@ -1,3 +1,246 @@
+// import { Component, AfterViewInit, NgZone, ViewChild, ElementRef } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { FloraService } from '../services/flora.service';
+// import { FaunaService } from '../services/fauna.service';
+
+// declare var Swiper: any;
+
+// @Component({
+//   selector: 'app-detail-page',
+//   templateUrl: './detail-page.component.html',
+//   styleUrls: ['./detail-page.component.css']
+// })
+// export class DetailPageComponent implements AfterViewInit {
+//   public flora: any;
+//   public fauna: any;
+//   public id: any;
+//   public type: any;
+//   private detailSwiper: any;
+//   private thumbsSwiper: any;
+
+//   constructor(
+//     private route: ActivatedRoute,
+//     private floraService: FloraService,
+//     private faunaService: FaunaService,
+//     private ngZone: NgZone
+//   ) { }
+
+//   ngOnInit(): void {
+//     this.ngZone.runOutsideAngular(() => {
+//       this.id = this.route.snapshot.paramMap.get('id');
+//       this.type = this.route.snapshot.paramMap.get('type');
+
+//       if (this.type === 'flora') {
+//         this.getFloraDetails();
+//       } else if (this.type === 'fauna') {
+//         this.getFaunaDetails();
+//       }
+//     });
+//   }
+
+//   ngAfterViewInit(): void {
+//     this.ngZone.runOutsideAngular(() => {
+//       this.thumbsSwiper = new Swiper('.mySwiperThumbs', {
+//         slidesPerView: 4,
+//         spaceBetween: 10,
+//         freeMode: true,
+//         watchSlidesVisibility: true,
+//         watchSlidesProgress: true,
+//       });
+  
+//       this.detailSwiper = new Swiper('.detailSwiper', {
+//         spaceBetween: 10,
+//         navigation: {
+//           nextEl: '.swiper-button-next',
+//           prevEl: '.swiper-button-prev',
+//         },
+//         thumbs: {
+//           swiper: this.thumbsSwiper,
+//         },
+//         breakpoints: {
+//           0: {
+//             slidesPerView: 1,
+//             spaceBetween: 10,
+//           },
+//           768: {
+//             slidesPerView: 1,
+//             spaceBetween: 20,
+//           },
+//           1024: {
+//             slidesPerView: 1,
+//             spaceBetween: 30,
+//           }
+//         }
+//       });
+//     });
+//   }
+
+//   getFloraDetails() {
+//     this.floraService.getFloraDetails(this.id).subscribe(
+//       (flora: any) => {
+//         this.ngZone.run(() => {
+//           this.flora = flora;
+//         });
+//       },
+//       error => {
+//         console.error('Error fetching flora details:', error);
+//       }
+//     );
+//   }
+
+//   getFaunaDetails() {
+//     this.faunaService.getFaunaDetails(this.id).subscribe(
+//       (fauna: any) => {
+//         this.ngZone.run(() => {
+//           this.fauna = fauna;
+//         });
+//       },
+//       error => {
+//         console.error('Error fetching fauna details:', error);
+//       }
+//     );
+//   }
+
+//   getImageUrl(imageData: any): string {
+//     if (imageData && imageData.data) {
+//       const blob = new Blob([new Uint8Array(imageData.data)], { type: imageData.contentType });
+//       return URL.createObjectURL(blob);
+//     }
+//     return '../../assets/Images/logoTahura.png';
+//   }
+
+//   handleImageError(event: any, photo: any) {
+//     console.error('Image loading error for photo:', photo, event);
+//   }
+
+//   updateMainPhoto(index: number) {
+//     if (this.detailSwiper) {
+//       this.detailSwiper.slideTo(index);
+//     }
+//   }
+// }
+
+
+// import { Component, AfterViewInit, NgZone, ViewChild, ElementRef } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { FloraService } from '../services/flora.service';
+// import { FaunaService } from '../services/fauna.service';
+
+// declare var Swiper: any;
+
+// @Component({
+//   selector: 'app-detail-page',
+//   templateUrl: './detail-page.component.html',
+//   styleUrls: ['./detail-page.component.css']
+// })
+// export class DetailPageComponent implements AfterViewInit {
+//   public flora: any;
+//   public fauna: any;
+//   public id: any;
+//   public type: any;
+//   private detailSwiper: any;
+//   private thumbsSwiper: any;
+
+//   constructor(
+//     private route: ActivatedRoute,
+//     private floraService: FloraService,
+//     private faunaService: FaunaService,
+//     private ngZone: NgZone
+//   ) { }
+
+//   ngOnInit(): void {
+//     this.id = this.route.snapshot.paramMap.get('id');
+//     this.type = this.route.snapshot.paramMap.get('type');
+
+//     if (this.type === 'flora') {
+//       this.getFloraDetails();
+//     } else if (this.type === 'fauna') {
+//       this.getFaunaDetails();
+//     }
+//   }
+
+//   ngAfterViewInit(): void {
+//     this.ngZone.runOutsideAngular(() => {
+//       this.thumbsSwiper = new Swiper('.mySwiperThumbs', {
+//         slidesPerView: 4,
+//         spaceBetween: 10,
+//         freeMode: true,
+//         watchSlidesVisibility: true,
+//         watchSlidesProgress: true,
+//       });
+
+//       this.detailSwiper = new Swiper('.detailSwiper', {
+//         spaceBetween: 10,
+//         navigation: {
+//           nextEl: '.swiper-button-next',
+//           prevEl: '.swiper-button-prev',
+//         },
+//         thumbs: {
+//           swiper: this.thumbsSwiper,
+//         },
+//         breakpoints: {
+//           0: {
+//             slidesPerView: 1,
+//             spaceBetween: 10,
+//           },
+//           768: {
+//             slidesPerView: 1,
+//             spaceBetween: 20,
+//           },
+//           1024: {
+//             slidesPerView: 1,
+//             spaceBetween: 30,
+//           }
+//         }
+//       });
+//     });
+//   }
+
+//   getFloraDetails() {
+//     this.floraService.getFloraDetails(this.id).subscribe(
+//       (flora: any) => {
+//         this.ngZone.run(() => {
+//           this.flora = flora;
+//         });
+//       },
+//       error => {
+//         console.error('Error fetching flora details:', error);
+//       }
+//     );
+//   }
+
+//   getFaunaDetails() {
+//     this.faunaService.getFaunaDetails(this.id).subscribe(
+//       (fauna: any) => {
+//         this.ngZone.run(() => {
+//           this.fauna = fauna;
+//         });
+//       },
+//       error => {
+//         console.error('Error fetching fauna details:', error);
+//       }
+//     );
+//   }
+
+//   getImageUrl(imageData: any): string {
+//     if (imageData && imageData.data) {
+//       const blob = new Blob([new Uint8Array(imageData.data)], { type: imageData.contentType });
+//       return URL.createObjectURL(blob);
+//     }
+//     return '../../assets/Images/logoTahura.png';
+//   }
+
+//   handleImageError(event: any, photo: any) {
+//     console.error('Image loading error for photo:', photo, event);
+//   }
+
+//   updateMainPhoto(index: number) {
+//     if (this.detailSwiper) {
+//       this.detailSwiper.slideTo(index);
+//     }
+//   }
+// }
+
 import { Component, AfterViewInit, NgZone, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FloraService } from '../services/flora.service';
@@ -15,7 +258,8 @@ export class DetailPageComponent implements AfterViewInit {
   public fauna: any;
   public id: any;
   public type: any;
-  private swiperInstance: any;
+  private detailSwiper: any;
+  private thumbsSwiper: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,48 +269,64 @@ export class DetailPageComponent implements AfterViewInit {
   ) { }
 
   ngOnInit(): void {
-    this.ngZone.runOutsideAngular(() => {
-      this.id = this.route.snapshot.paramMap.get('id');
-      this.type = this.route.snapshot.paramMap.get('type');
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.type = this.route.snapshot.paramMap.get('type');
 
-      if (this.type === 'flora') {
-        this.getFloraDetails();
-      } else if (this.type === 'fauna') {
-        this.getFaunaDetails();
-      }
-    });
+    if (this.type === 'flora') {
+      this.getFloraDetails();
+    } else if (this.type === 'fauna') {
+      this.getFaunaDetails();
+    }
   }
 
   ngAfterViewInit(): void {
     this.ngZone.runOutsideAngular(() => {
-      this.swiperInstance = new Swiper('.container3', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        loop: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        breakpoints: {
-          0: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          768: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          1024: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-          }
-        }
-      });
+      this.initSwipers();
     });
+  }
+
+  initSwipers() {
+    if ((this.flora?.photos && this.flora.photos.length > 1) || 
+        (this.fauna?.photos && this.fauna.photos.length > 1)) {
+
+      if (!this.thumbsSwiper || !this.detailSwiper) {
+        this.thumbsSwiper = new Swiper('.mySwiperThumbs', {
+          slidesPerView: 4,
+          spaceBetween: 10,
+          freeMode: true,
+          watchSlidesVisibility: true,
+          watchSlidesProgress: true,
+        });
+
+        this.detailSwiper = new Swiper('.detailSwiper', {
+          spaceBetween: 10,
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+          thumbs: {
+            swiper: this.thumbsSwiper,
+          },
+          breakpoints: {
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 1,
+              spaceBetween: 30,
+            }
+          }
+        });
+      } else {
+        this.detailSwiper.update();
+        this.thumbsSwiper.update();
+      }
+    }
   }
 
   getFloraDetails() {
@@ -74,6 +334,7 @@ export class DetailPageComponent implements AfterViewInit {
       (flora: any) => {
         this.ngZone.run(() => {
           this.flora = flora;
+          this.initSwipers();
         });
       },
       error => {
@@ -87,6 +348,7 @@ export class DetailPageComponent implements AfterViewInit {
       (fauna: any) => {
         this.ngZone.run(() => {
           this.fauna = fauna;
+          this.initSwipers();
         });
       },
       error => {
@@ -108,8 +370,8 @@ export class DetailPageComponent implements AfterViewInit {
   }
 
   updateMainPhoto(index: number) {
-    if (this.swiperInstance) {
-      this.swiperInstance.slideTo(index);
+    if (this.detailSwiper) {
+      this.detailSwiper.slideTo(index);
     }
   }
 }
